f8eb884a5cfd0368f0bcdd376c4648577442d4ca
siddharthav19 <106438627+siddharthav19@users.noreply.github.com>
2022-06-14 22:02:43 +0530

    Update index1.js

--- index1.js
+++ index1.js
@@ -28,7 +28,7 @@ if(JSON.parse(localStorage.getItem('todo')).length !== 0)
 {
     ulParent.innerHTML = '';
     JSON.parse(localStorage.getItem('todo')).forEach((el)=>{
-        // console.log(el);
+       
         ulParent.innerHTML += `<li class="list-group-item d-flex justify-content-between align-items-center">
      <span>${el}</span>
      <i class="far fa-trash-alt delete"></i>`;
@@ -57,13 +57,10 @@ const removeFromList = (val) =>{
     let lis = JSON.parse(localStorage.getItem('todo'));
 
     lis.splice(inx,1);
-    // console.log(lis);
+  
 
     localStorage.setItem('todo',JSON.stringify(lis));
 
-
-
-    // console.log(todosList);
 }
 
 const generateTemplate = (todo)=>{
@@ -73,8 +70,6 @@ const generateTemplate = (todo)=>{
      <i class="far fa-trash-alt delete"></i>
      `;
 
-
-    //     ulParent.append(html);   // do not use this here in this case bcz if u want to use append first u need to create a element and add it
     ulParent.innerHTML+=html;
     addToList(todo);
 
@@ -88,33 +83,13 @@ addTodo.addEventListener('submit',(e)=>{
 
     e.preventDefault();
 
-    /*
-
-              e.preventDefault();
-
-
-         let newElement = document.createElement('span');
-         newElement.innerHTML = `<li class="list-group-item d-flex justify-content-between align-items-center">
-         <span>make a veggie pie</span>
-         <i class="far fa-trash-alt delete"></i>`;
-
-         newElement.querySelector('span').textContent = addTodo.add.value.trim();
-         // console.log(newElement.textContent);
-         document.querySelector('ul').append(newElement);
-
-         addTodo.reset();  //reset method is for clearing the input field this reset method resets the input field's in the respective forms
-
-
-    */
+   
     const input = addTodo.add.value.trim();
 
     if(input.length)
     {
         generateTemplate(input);
-        // addTodo.reset(); //you can add this here or down also
-        // reset method is for clearing the input field this reset method resets the input field's in the respective forms
-
-
+        
     }
 
     addTodo.reset();
@@ -127,11 +102,10 @@ addTodo.addEventListener('submit',(e)=>{
 
 
 ulParent.addEventListener('click',(e)=>{
-    // console.log(e.target.tagName);
-
-    if(e.target.classList.contains('delete')) //classList.contains('delete') returns true if it has class delete
+   
+    if(e.target.classList.contains('delete')) 
     {
-        // console.log(e.target.previousElementSibling.textContent);
+       
         removeFromList(e.target.previousElementSibling.textContent);
         e.target.parentElement.remove();
 
@@ -141,17 +115,13 @@ ulParent.addEventListener('click',(e)=>{
 });
 
 
-//key up for search
-
-
-
 
 
 const Filterup = (term)=>{
 
 
 
-    //hiding the unmatched
+   
     Array.from(list).filter((val)=>{
 
         return !(val.textContent.toLowerCase().includes(term));
@@ -159,9 +129,6 @@ const Filterup = (term)=>{
         through.classList.add('searchFilter')
     });
 
-
-
-    //making visible the hidden
 
     Array.from(list).filter((val)=>{
 
@@ -175,21 +142,6 @@ const Filterup = (term)=>{
 searchEngine.addEventListener('keyup',(e)=>{
 
     const inputVal = searchEngine.value.toLowerCase().trim();
-    // console.log(inputVal);
+   
     Filterup(inputVal);
 });
-
-// searchEngine.addEventListener('keyup',(e)=>{
-//      const text = searchEngine.value.toLowerCase().trim();
-//      Array.from(list).filter((val)=>{
-//           return !val.textContent.toLowerCase().includes(text);
-//      }).forEach((ele)=>{                                                      //same function as above but written whole code in one place
-//           ele.classList.add('searchFilter');
-//      });
-//      Array.from(list).filter((val)=>{
-//           return val.textContent.toLowerCase().includes(text);
-//      }).forEach((ele)=>{
-//           ele.classList.remove('searchFilter');
-//      });
-
-// });
